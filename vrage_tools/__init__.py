75055d734c452200d750d45068120423f57836ab
quantum-unicorn <70114668+quantum-unicorn@users.noreply.github.com>
2025-11-28 18:45:20 +0100

    version bump

--- vrage_tools/__init__.py
+++ vrage_tools/__init__.py
@@ -20,7 +20,7 @@ bl_info = {
     "author" : "Keen Software House",
     "description" : "A Blender Add-on to streamline and simplify the creation of 3D assets for Space Engineers 2",
     "blender" : (4, 3, 0),
-    "version" : (0, 3, 0),
+    "version" : (0, 3, 1),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
